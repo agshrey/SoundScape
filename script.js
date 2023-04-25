60648259822d6dbeb8f0f77f3f14297bcfe5bca4
Shrey Agarwal <agshrey@gmail.com>
2023-04-24 23:29:46 -0400

    got basic visualization working, spotify oauth next

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let audioSource;
 let analyser;
 
 container.addEventListener("click", function() {
-    let audio1 = new Audio("audio.wav");
+    let audio1 = new Audio("hindi.mp3");
     const audioContext = new (window.AudioContext)();
     audio1.play();
     audioSource = audioContext.createMediaElementSource(audio1);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioContext.destination);
-    analyser.fftSize = 64;
+    analyser.fftSize = 128;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
@@ -28,12 +28,17 @@ container.addEventListener("click", function() {
         ctx.clearRect(0,0,canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
         for(let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i]*2;
+            barHeight = dataArray[i]*1.5;
             ctx.fillStyle = "white";
             ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x+= barWidth;
+            x+= barWidth+10;
         }
         requestAnimationFrame(animate);
     }
     animate();
+})
+
+const spotifyButton = document.getElementById("spotify");
+spotifyButton.addEventListener("click", function() {
+    
 })
